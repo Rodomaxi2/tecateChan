224e4799ae36c25db544ac0922dc61322086efd8
Rodolfo Maximo <rodomaxi2@gmail.com>
2019-11-05 09:43:13 -0600

    Se agrego la clase SisMotores para controlarlos en conjunto

---
+++ SisMotores.h
@@ -0,0 +1,16 @@
+#ifndef SisMotores_H
+#define SisMotores_H
+
+#include "Motor.h"
+
+class SisMotores
+{
+    public:
+        SisMotores(Motor L1, Motor L2, Motor R1, Motor R2)
+        {
+            //
+        }
+};
+
+
+#endif
