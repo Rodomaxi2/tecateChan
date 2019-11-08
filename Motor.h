35c56ae0524a11cf14e9d755ae61197aa48cad1c
Rodolfo Maximo <rodomaxi2@gmail.com>
2019-11-08 11:04:58 -0600

    Eliminado Motor.cpp para poder tener menos archivos

--- Motor.h
+++ Motor.h
@@ -8,12 +8,60 @@ class Motor
   public:
     int pinA, pinB, EN; //se declaran las variables basicas
     
-    Motor(int _EN, int _pinA, int _pinB);
-    Motor* 	asignar(Motor *from);
-    void adelante();
-    void atras();
-    void parar();
-    void parar(int);
+
+    Motor(int _EN = 0, int _pinA = 0, int _pinB = 0) //Constructor que asigna los pines, 0 por defecto
+     {
+        pinA = _pinA;
+        pinB = _pinB;
+        EN = _EN;
+
+        //Se configuran los pines como salidas
+        pinMode(EN, OUTPUT);
+  		pinMode(pinA, OUTPUT);
+  		pinMode(pinB, OUTPUT);
+     } 
+
+	void adelante() //avanza por tiempo indefinido
+	{
+		//parar(100);
+		//Direccion de motor
+		digitalWrite(pinA, HIGH);
+		digitalWrite(pinB, LOW);
+		analogWrite(EN,255); //velocidad de motor
+	}
+
+	void atras() //da marcha atras por tiempo indefinido
+	{
+		//parar(100);
+		//Direccion de motor
+		digitalWrite(pinA, LOW);
+		digitalWrite(pinB, HIGH);
+		analogWrite(EN,128); //velocidad de motor
+	}
+
+	void parar() //Detiene el motor por tiempo indefinido
+	{
+		//Direccion de motor
+		digitalWrite(pinA, LOW);
+		digitalWrite(pinB, LOW);
+		analogWrite(EN, 0); //Velocidad de motor
+	}
+
+	void parar(int tiempo) //Funcion parar que recibe un valor que recibe el tiempo que detiene los motores y despues hace un delay por el tiempo indicado
+	{
+		//Direccion de motor
+		digitalWrite(pinA, LOW);
+		digitalWrite(pinB, LOW);
+		analogWrite(EN, 0); //Velocidad de motor
+		delay(tiempo);
+	}
+
+
+	Motor* asignar(Motor *from)
+	{
+		Motor *rtn = new Motor(from ->EN, from-> pinA, from->pinB);
+		return rtn;
+	}
 };
 
 #endif
