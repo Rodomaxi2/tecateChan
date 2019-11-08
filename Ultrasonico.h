1ee6eda2b4b68012935820952578273708ed9854
Rodolfo Maximo <rodomaxi2@gmail.com>
2019-11-08 11:11:25 -0600

    Formato de codigo

--- Ultrasonico.h
+++ Ultrasonico.h
@@ -10,6 +10,27 @@ class Ultrasonico
 
 		Ultrasonico(int, int);
 		float distancia();
+
+		Ultrasonico::Ultrasonico(int _trigger, int _echo)
+		{
+			trigger = _trigger;
+			echo = _echo;
+
+		  pinMode(trigger, OUTPUT);//pin como salida
+		  pinMode(echo, INPUT);//pin como entrada 
+		  digitalWrite(trigger, LOW);//inicializamos el pin con 0
+		}
+
+		float Ultrasonico::distancia()
+		{
+			float t, d;//timepo en que demora en llegar el eco y distancia en cm
+		  digitalWrite(trigger,HIGH);
+		  delayMicroseconds(10); //enviamos un pulso de 10 us
+		  digitalWrite(trigger, LOW);
+		  t = pulseIn(echo, HIGH);//obtenemos el anch del pulso
+		  d = t/59;//escalamo el tiempo a una distancia en cm
+		  return d;
+		}
 };
 
 #endif
